a0e0e67dcaf841eb9e9d29d7dbb1643835e65e5b
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-11-16 22:52:18 +0530

    working on postman api

--- backend/Data/data.js
+++ backend/Data/data.js
@@ -106,3 +106,4 @@ const chats = [
     },
   },
 ];
+module.exports = { chats };
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const { chats } = require("./Data/data");
 
 const app = express();
 
@@ -6,5 +9,15 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get();
-app.listen(5000, console.log("Server Started on Port 5000"));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+// app.get("/api/chat/:id", (req, res) => {
+//   console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.is);
+//   res.send(singleChat);
+// });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
