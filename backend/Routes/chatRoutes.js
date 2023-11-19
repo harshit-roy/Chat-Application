4e2909bc826d4ec0fea9ba608f51f76a03ad4401
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-11-19 12:07:39 +0530

    created Chat Api

--- backend/Routes/chatRoutes.js
+++ backend/Routes/chatRoutes.js
@@ -6,16 +6,16 @@ const {
   removeFromGroup,
   addToGroup,
   renameGroup,
-} = require("../controllers/chatControllers");
-const { protect } = require("../middleware/authMiddleware");
+} = require("../controllers/chatController");
+const { protect } = require("../middlewares/authmiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, renameGroup);
-router.route("/groupremove").put(protect, removeFromGroup);
-router.route("/groupadd").put(protect, addToGroup);
+// router.route("/group").post(protect, createGroupChat);
+// router.route("/rename").put(protect, renameGroup);
+// router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
